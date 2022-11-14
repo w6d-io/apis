6f714a6137c6690eb87345a80f68d97398f545a3
David ALEXANDRE <david.alexandre@w6d.io>
2022-11-14 20:10:12 +0100

    break: change condition to when api into tekton

--- pipeline/v1alpha1/helper_test.go
+++ pipeline/v1alpha1/helper_test.go
@@ -1,89 +1,126 @@
 package v1alpha1_test
 
 import (
-	"fmt"
-	apis "github.com/w6d-io/apis/pipeline/v1alpha1"
+    "fmt"
 
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
+    . "github.com/onsi/ginkgo"
+    . "github.com/onsi/gomega"
+    "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
 
-	"github.com/w6d-io/x/errorx"
+    apis "github.com/w6d-io/apis/pipeline/v1alpha1"
+    "github.com/w6d-io/x/errorx"
 )
 
 var _ = Describe("Pipelines", func() {
-	Context("Helper", func() {
-		It("Succeed the validation", func() {
-			p := &apis.Pipeline{
-				Triggers: []apis.Trigger{{ID: "trigger-1"}},
-				Stages: []apis.Stage{
-					{
-						ID: "stage-1",
-						Tasks: []apis.Task{
-							{
-								ID:         "task-1",
-								Conditions: [][]apis.Condition{{{Ref: "trigger-1"}}},
-							},
-						},
-					},
-				},
-			}
-			Expect(p.ConditionValidation()).To(Succeed())
-		})
-		It("Failed the validation", func() {
-			p := &apis.Pipeline{
-				Triggers: []apis.Trigger{{ID: "trigger-1"}},
-				Stages: []apis.Stage{
-					{
-						ID: "stage-1",
-						Tasks: []apis.Task{
-							{
-								ID:         "task-1",
-								Conditions: [][]apis.Condition{{{Ref: "task-2"}}},
-							},
-						},
-					},
-				},
-			}
-			Expect(p.ConditionValidation()).To(Equal(errorx.New(nil, "task-2")))
-		})
-		It("Succeed the validation on empty pipeline", func() {
-			p := &apis.Pipeline{}
-			Expect(p.ConditionValidation()).To(Succeed())
-		})
-		It("Failed multiple validation", func() {
-			p := &apis.Pipeline{
-				Triggers: []apis.Trigger{{ID: "trigger-1"}},
-				Stages: []apis.Stage{
-					{
-						ID: "stage-1",
-						Tasks: []apis.Task{
-							{
-								ID:         "task-1",
-								Conditions: [][]apis.Condition{{{Ref: "task-20"}}},
-							},
-							{
-								ID:         "task-2",
-								Conditions: [][]apis.Condition{{{Ref: "task-30"}}},
-							},
-							{
-								ID:         "task-3",
-								Conditions: [][]apis.Condition{{{Ref: "trigger-1"}}},
-							},
-						},
-					},
-					{
-						ID: "stage-2",
-						Tasks: []apis.Task{
-							{
-								ID:         "task-1",
-								Conditions: [][]apis.Condition{{{Ref: "stage-1"}}},
-							},
-						},
-					},
-				},
-			}
-			fmt.Println(p.ConditionValidation())
-			Expect(p.ConditionValidation()).To(Equal(errorx.New(nil, "task-20, task-30")))
-		})
-	})
+    Context("Helper", func() {
+        It("Succeed the validation", func() {
+            p := &apis.Pipeline{
+                Triggers: []apis.Trigger{{ID: "trigger-1"}},
+                Stages: []apis.Stage{
+                    {
+                        ID: "stage-1",
+                        Tasks: []apis.Task{
+                            {
+                                ID: "task-1",
+                                Conditions: v1beta1.WhenExpressions{
+                                    {
+                                        Input:    "trigger.name",
+                                        Operator: "in",
+                                        Values:   []string{"trigger-1"},
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+            }
+            Expect(p.ConditionValidation()).To(Succeed())
+        })
+        It("Failed the validation", func() {
+            p := &apis.Pipeline{
+                Triggers: []apis.Trigger{{ID: "trigger-1"}},
+                Stages: []apis.Stage{
+                    {
+                        ID: "stage-1",
+                        Tasks: []apis.Task{
+                            {
+                                ID: "task-1",
+                                Conditions: v1beta1.WhenExpressions{
+                                    {
+                                        Input:    "task.name",
+                                        Operator: "in",
+                                        Values:   []string{"task-2"},
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+            }
+            Expect(p.ConditionValidation()).To(Equal(errorx.New(nil, "task-2")))
+        })
+        It("Succeed the validation on empty pipeline", func() {
+            p := &apis.Pipeline{}
+            Expect(p.ConditionValidation()).To(Succeed())
+        })
+        It("Failed multiple validation", func() {
+            p := &apis.Pipeline{
+                Triggers: []apis.Trigger{{ID: "trigger-1"}},
+                Stages: []apis.Stage{
+                    {
+                        ID: "stage-1",
+                        Tasks: []apis.Task{
+                            {
+                                ID: "task-1",
+                                Conditions: v1beta1.WhenExpressions{
+                                    {
+                                        Input:    "task.name",
+                                        Operator: "in",
+                                        Values:   []string{"task-20"},
+                                    },
+                                },
+                            },
+                            {
+                                ID: "task-2",
+                                Conditions: v1beta1.WhenExpressions{
+                                    {
+                                        Input:    "task.name",
+                                        Operator: "in",
+                                        Values:   []string{"task-30"},
+                                    },
+                                },
+                            },
+                            {
+                                ID: "task-3",
+                                Conditions: v1beta1.WhenExpressions{
+                                    {
+                                        Input:    "trigger.name",
+                                        Operator: "in",
+                                        Values:   []string{"trigger-1"},
+                                    },
+                                },
+                            },
+                        },
+                    },
+                    {
+                        ID: "stage-2",
+                        Tasks: []apis.Task{
+                            {
+                                ID: "task-1",
+                                Conditions: v1beta1.WhenExpressions{
+                                    {
+                                        Input:    "stage.name",
+                                        Operator: "in",
+                                        Values:   []string{"stage-1"},
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+            }
+            fmt.Println(p.ConditionValidation())
+            Expect(p.ConditionValidation()).To(Equal(errorx.New(nil, "task-20, task-30")))
+        })
+    })
 })
