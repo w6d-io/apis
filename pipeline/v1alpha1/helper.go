61125969e3f11c923ceb1ebbca465d1b833fd66c
David ALEXANDRE <david.alexandre@w6d.io>
2023-12-26 17:02:45 +0100

    chore: bump dependencies version

--- pipeline/v1alpha1/helper.go
+++ pipeline/v1alpha1/helper.go
@@ -16,43 +16,43 @@ Created on 24/10/2022
 package v1alpha1
 
 import (
-    "github.com/w6d-io/x/errorx"
-    "github.com/w6d-io/x/toolx"
-    "strings"
+	"github.com/w6d-io/x/errorx"
+	"github.com/w6d-io/x/toolx"
+	"strings"
 )
 
 func (in Conditions) Has(triggersID []string) []string {
-    var errorMessages []string
-    for _, cs := range in {
-        for _, c := range cs {
-            if !toolx.InArray(c.Ref, triggersID) {
-                errorMessages = append(errorMessages, c.Ref)
-            }
-        }
-    }
-    return errorMessages
+	var errorMessages []string
+	for _, cs := range in {
+		for _, c := range cs {
+			if !toolx.InArray(c.Ref, triggersID) {
+				errorMessages = append(errorMessages, c.Ref)
+			}
+		}
+	}
+	return errorMessages
 }
 
 func (in *Pipeline) ConditionValidation() error {
 
-    var errorMessages []string
-    var triggersID []string
+	var errorMessages []string
+	var triggersID []string
 
-    for _, t := range in.Triggers {
-        triggersID = append(triggersID, t.ID)
-    }
+	for _, t := range in.Triggers {
+		triggersID = append(triggersID, t.ID)
+	}
 
-    for _, s := range in.Stages {
-        var stagesTasksID []string
-        stagesTasksID = append(stagesTasksID, s.ID)
-        for _, t := range s.Tasks {
-            stagesTasksID = append(stagesTasksID, t.ID)
-            errorMessages = append(errorMessages, t.Conditions.Has(triggersID)...)
-        }
-        triggersID = append(triggersID, stagesTasksID...)
-    }
-    if len(errorMessages) != 0 {
-        return errorx.New(nil, strings.Join(errorMessages, ", "))
-    }
-    return nil
+	for _, s := range in.Stages {
+		var stagesTasksID []string
+		stagesTasksID = append(stagesTasksID, s.ID)
+		for _, t := range s.Tasks {
+			stagesTasksID = append(stagesTasksID, t.ID)
+			errorMessages = append(errorMessages, t.Conditions.Has(triggersID)...)
+		}
+		triggersID = append(triggersID, stagesTasksID...)
+	}
+	if len(errorMessages) != 0 {
+		return errorx.New(strings.Join(errorMessages, ", "))
+	}
+	return nil
 }
--- pipeline/v1alpha1/pipeline.go
+++ pipeline/v1alpha1/pipeline.go
@@ -18,121 +18,121 @@ Created on 15/12/2021
 package v1alpha1
 
 import (
-    "fmt"
+	"fmt"
 )
 
 type Status int
 
 const (
-    Pending Status = iota
-    Running
-    Cancelled
-    Skipped
-    Timeout
+	Pending Status = iota
+	Running
+	Cancelled
+	Skipped
+	Timeout
 )
 
 // PipelineStatus string list of status for pipeline
 var PipelineStatus = []string{
-    "Pending",
-    "Running",
-    "Cancelled",
-    "Skipped",
-    "Timeout",
+	"Pending",
+	"Running",
+	"Cancelled",
+	"Skipped",
+	"Timeout",
 }
 
 func (s Status) ToString() string {
-    return PipelineStatus[s]
+	return PipelineStatus[s]
 }
 
 type Data map[string]string
 
 type Trigger struct {
-    ID          string `json:"id"          bson:"id"          mapstructure:"id"`
-    Ref         string `json:"ref"         bson:"ref"         mapstructure:"ref"`
-    Type        string `json:"type"        bson:"type"        mapstructure:"type"`
-    ComponentId string `json:"componentId" bson:"componentId" mapstructure:"componentId"`
-    Data        Data   `json:"data"        bson:"data"        mapstructure:"data"`
+	ID          string `json:"id"          bson:"id"          mapstructure:"id"`
+	Ref         string `json:"ref"         bson:"ref"         mapstructure:"ref"`
+	Type        string `json:"type"        bson:"type"        mapstructure:"type"`
+	ComponentId string `json:"componentId" bson:"componentId" mapstructure:"componentId"`
+	Data        Data   `json:"data"        bson:"data"        mapstructure:"data"`
 }
 
 type ConditionType string
 
 const (
-    TRIGGER ConditionType = "trigger"
-    TASK    ConditionType = "task"
-    STAGE   ConditionType = "stage"
+	TRIGGER ConditionType = "trigger"
+	TASK    ConditionType = "task"
+	STAGE   ConditionType = "stage"
 )
 
 type Condition struct {
-    Id   string        `json:"id" bson:"id"`
-    Ref  string        `json:"ref" bson:"ref"`
-    Type ConditionType `json:"type" bson:"type"`
-    When string        `json:"when" bson:"when"`
+	Id   string        `json:"id" bson:"id"`
+	Ref  string        `json:"ref" bson:"ref"`
+	Type ConditionType `json:"type" bson:"type"`
+	When string        `json:"when" bson:"when"`
 }
 
 type Conditions [][]Condition
 
 type Action struct {
-    ID           string            `json:"id"           bson:"id"           mapstructure:"id"`
-    Name         string            `json:"name"         bson:"name"         mapstructure:"name"`
-    ComponentID  string            `json:"componentId"  bson:"componentId"  mapstructure:"componentId"`
-    Ref          string            `json:"ref"          bson:"ref"          mapstructure:"ref"`
-    Data         map[string]string `json:"data"         bson:"data"         mapstructure:"data"`
-    Params       map[string]string `json:"params"       bson:"params"       mapstructure:"params"`
-    Environments map[string]string `json:"environments" bson:"environments" mapstructure:"environments"`
-    Status       string            `json:"status"       bson:"status"       mapstructure:"status"`
-    StartTime    int64             `json:"startTime"    bson:"startTime"    mapstructure:"startTime"`
-    EndTime      int64             `json:"endTime"      bson:"endTime"      mapstructure:"endTime"`
+	ID           string            `json:"id"           bson:"id"           mapstructure:"id"`
+	Name         string            `json:"name"         bson:"name"         mapstructure:"name"`
+	ComponentID  string            `json:"componentId"  bson:"componentId"  mapstructure:"componentId"`
+	Ref          string            `json:"ref"          bson:"ref"          mapstructure:"ref"`
+	Data         map[string]string `json:"data"         bson:"data"         mapstructure:"data"`
+	Params       map[string]string `json:"params"       bson:"params"       mapstructure:"params"`
+	Environments map[string]string `json:"environments" bson:"environments" mapstructure:"environments"`
+	Status       string            `json:"status"       bson:"status"       mapstructure:"status"`
+	StartTime    int64             `json:"startTime"    bson:"startTime"    mapstructure:"startTime"`
+	EndTime      int64             `json:"endTime"      bson:"endTime"      mapstructure:"endTime"`
 }
 
 type Task struct {
-    ID            string     `json:"id"            bson:"id"            mapstructure:"id"`
-    Name          string     `json:"name"          bson:"name"          mapstructure:"name"`
-    SkipOnFailure bool       `json:"skipOnFailure" bson:"skipOnFailure" mapstructure:"skipOnFailure"`
-    Conditions    Conditions `json:"conditions"    bson:"conditions"    mapstructure:"conditions"`
-    Actions       []Action   `json:"actions"       bson:"actions"       mapstructure:"actions"`
-    StartTime     int64      `json:"startTime"     bson:"startTime"     mapstructure:"startTime"`
-    EndTime       int64      `json:"endTime"       bson:"endTime"       mapstructure:"endTime"`
-    Status        string     `json:"status"        bson:"status"        mapstructure:"status"`
+	ID            string     `json:"id"            bson:"id"            mapstructure:"id"`
+	Name          string     `json:"name"          bson:"name"          mapstructure:"name"`
+	SkipOnFailure bool       `json:"skipOnFailure" bson:"skipOnFailure" mapstructure:"skipOnFailure"`
+	Conditions    Conditions `json:"conditions"    bson:"conditions"    mapstructure:"conditions"`
+	Actions       []Action   `json:"actions"       bson:"actions"       mapstructure:"actions"`
+	StartTime     int64      `json:"startTime"     bson:"startTime"     mapstructure:"startTime"`
+	EndTime       int64      `json:"endTime"       bson:"endTime"       mapstructure:"endTime"`
+	Status        string     `json:"status"        bson:"status"        mapstructure:"status"`
 }
 
 type Stage struct {
-    ID        string `json:"id"         bson:"id"         mapstructure:"id"`
-    Name      string `json:"name"       bson:"name"       mapstructure:"name"`
-    Tasks     []Task `json:"tasks"      bson:"tasks"      mapstructure:"tasks"`
-    Status    string `json:"status"     bson:"status"     mapstructure:"status"`
-    EndTime   int64  `json:"endTime"    bson:"endTime"    mapstructure:"endTime"`
-    StartTime int64  `json:"startTime"  bson:"startTime"  mapstructure:"startTime"`
+	ID        string `json:"id"         bson:"id"         mapstructure:"id"`
+	Name      string `json:"name"       bson:"name"       mapstructure:"name"`
+	Tasks     []Task `json:"tasks"      bson:"tasks"      mapstructure:"tasks"`
+	Status    string `json:"status"     bson:"status"     mapstructure:"status"`
+	EndTime   int64  `json:"endTime"    bson:"endTime"    mapstructure:"endTime"`
+	StartTime int64  `json:"startTime"  bson:"startTime"  mapstructure:"startTime"`
 }
 
 type ProjectID int64
 
 func (in ProjectID) String() string {
-    return fmt.Sprintf("%d", in)
+	return fmt.Sprintf("%d", in)
 }
 
 // Pipeline defines the desired state of Pipeline
 type Pipeline struct {
-    ID               string    `json:"id"                  bson:"id"                mapstructure:"id"`
-    Type             string    `json:"type"                bson:"type"              mapstructure:"type"`
-    PipelineIDNumber string    `json:"pipelineIdNumber"    bson:"pipelineIdNumber"  mapstructure:"pipelineIdNumber"`
-    ProjectID        ProjectID `json:"projectId"           bson:"projectId"         mapstructure:"projectId"`
-    Name             string    `json:"name"                bson:"name"              mapstructure:"name"`
-    Triggers         []Trigger `json:"triggers"            bson:"triggers"          mapstructure:"triggers"`
-    Stages           []Stage   `json:"stages"              bson:"stages"            mapstructure:"stages"`
-    Status           string    `json:"status"              bson:"status"            mapstructure:"status"`
-    StartTime        int64     `json:"startTime"           bson:"startTime"         mapstructure:"startTime"`
-    EndTime          int64     `json:"endTime"             bson:"endTime"           mapstructure:"endTime"`
-    LogUri           string    `json:"logUri"              bson:"logUri"            mapstructure:"logUri"`
-    Complete         bool      `json:"complete"            bson:"complete"          mapstructure:"complete"`
-    Force            bool      `json:"force"               bson:"-"                 mapstructure:"-"`
-    Artifacts        bool      `json:"artifacts"           bson:"artifacts"         mapstructure:"artifacts"`
-    TriggerId        string    `json:"triggerId,omitempty" bson:"triggerId"         mapstructure:"triggerId"`
-    Commit           Commit    `json:"commit"              bson:"commit"            mapstructure:"commit"`
-    EventID          string    `json:"eventId"             bson:"eventId"           mapstructure:"eventId"`
+	ID               string    `json:"id"                  bson:"id"                mapstructure:"id"`
+	Type             string    `json:"type"                bson:"type"              mapstructure:"type"`
+	PipelineIDNumber string    `json:"pipelineIdNumber"    bson:"pipelineIdNumber"  mapstructure:"pipelineIdNumber"`
+	ProjectID        ProjectID `json:"projectId"           bson:"projectId"         mapstructure:"projectId"`
+	Name             string    `json:"name"                bson:"name"              mapstructure:"name"`
+	Triggers         []Trigger `json:"triggers"            bson:"triggers"          mapstructure:"triggers"`
+	Stages           []Stage   `json:"stages"              bson:"stages"            mapstructure:"stages"`
+	Status           string    `json:"status"              bson:"status"            mapstructure:"status"`
+	StartTime        int64     `json:"startTime"           bson:"startTime"         mapstructure:"startTime"`
+	EndTime          int64     `json:"endTime"             bson:"endTime"           mapstructure:"endTime"`
+	LogUri           string    `json:"logUri"              bson:"logUri"            mapstructure:"logUri"`
+	Complete         bool      `json:"complete"            bson:"complete"          mapstructure:"complete"`
+	Force            bool      `json:"force"               bson:"-"                 mapstructure:"-"`
+	Artifacts        bool      `json:"artifacts"           bson:"artifacts"         mapstructure:"artifacts"`
+	TriggerId        string    `json:"triggerId,omitempty" bson:"triggerId"         mapstructure:"triggerId"`
+	Commit           Commit    `json:"commit"              bson:"commit"            mapstructure:"commit"`
+	EventID          string    `json:"eventId"             bson:"eventId"           mapstructure:"eventId"`
 }
 
 type Commit struct {
-    ID      string `json:"id"      bson:"id"      mapstructure:"id"`
-    Ref     string `json:"ref"     bson:"ref"     mapstructure:"ref"`
-    Message string `json:"message" bson:"message" mapstructure:"message"`
+	ID      string `json:"id"      bson:"id"      mapstructure:"id"`
+	Ref     string `json:"ref"     bson:"ref"     mapstructure:"ref"`
+	Message string `json:"message" bson:"message" mapstructure:"message"`
 }
